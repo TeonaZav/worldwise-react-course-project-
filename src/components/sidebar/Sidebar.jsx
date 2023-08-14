d407e8c895ff23dba27e752f1edcd34f4ff08eac
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-08-14 18:10:51 +0400

    create sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,14 +2,14 @@ import AppNav from "../appnav/AppNav";
 import Logo from "../logo/Logo";
 import styles from "./Sidebar.module.css";
 import Footer from "../footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
-
+      <Outlet />
       <Footer />
     </div>
   );
