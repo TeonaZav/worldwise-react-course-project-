6e4e5d603aa4a831610e646390bf9c7d7b56f6af
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-08-21 19:40:24 +0400

    feat: add Map component. use 'react-leaflet'

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,7 +1,72 @@
+import { useEffect, useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import styles from "./Map.module.css";
+import { useCities } from "../../contexts/CitiesContext";
+import { flagemojiToPNG } from "../../utils/HelperFunctions";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const [searchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(() => {
+    if ((mapLat, mapLng)) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
+
+  return (
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city, index) => {
+          return (
+            <Marker
+              position={[city.position.lat, city.position.lng]}
+              key={`${city.id}_${index}`}
+            >
+              <Popup>
+                {city.emoji && <span>{flagemojiToPNG(city.emoji)}</span>}
+                {city.cityName} <span></span>
+              </Popup>
+            </Marker>
+          );
+        })}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
+    </div>
+  );
+}
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => {
+      console.log(e);
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
