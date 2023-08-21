1cc11c7a4a235c8391908cf6dd60a2bdd98d67d8
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-08-21 20:05:42 +0400

    feat: add useGeolocation custom hook

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -11,11 +11,20 @@ import {
 import styles from "./Map.module.css";
 import { useCities } from "../../contexts/CitiesContext";
 import { flagemojiToPNG } from "../../utils/HelperFunctions";
+import { useGeolocation } from "../../hooks/useGeolocation";
+import Button from "../buttons/Button";
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams] = useSearchParams();
+
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -23,8 +32,18 @@ function Map() {
     if ((mapLat, mapLng)) setMapPosition([mapLat, mapLng]);
   }, [mapLat, mapLng]);
 
+  useEffect(() => {
+    if (geolocationPosition)
+      setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+  }, [geolocationPosition]);
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
