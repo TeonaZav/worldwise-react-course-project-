7cadbd160885ed3cb9d66707190c99cb021ad57d
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-09-29 22:00:26 +0400

    cleanup

--- src/pages/applayout/AppLayout.jsx
+++ src/pages/applayout/AppLayout.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import Map from "../../components/map/Map";
 import styles from "./AppLayout.module.css";
+import User from "../../components/user/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
